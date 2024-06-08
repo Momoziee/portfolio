78817c4ec2b205ffa2d43a5f14630898669c050e
Muhammad Abdoola <momoawsome55@gmail.com>
2024-06-09 00:38:27 +0200

    made a full bento grid section to map elements as well as inculded all styling and yes that took long

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import {BentoGrid, BentoGridItem} from "@/components/ui/BentoGrid";
+import {gridItems} from "@/data";
 
 const Grid = () => {
     return (
-        <div>
-            
-        </div>
+        <section id ="about">
+            <BentoGrid>
+                {gridItems.map(({id,title,description
+                                    ,className,imgClassName,
+                                    img,titleClassName,spareImg}) =>(
+                    <BentoGridItem
+
+                        id={id}
+                        key={id}
+                        title={title}
+                        description={description}
+                        className={className}
+                        img={img}
+                        imgClassName={imgClassName}
+                        titleClassName={titleClassName}
+                        spareImg={spareImg}
+
+                    />
+
+                    ))}
+            </BentoGrid>
+
+        </section>
     )
 }
 
