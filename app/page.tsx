50e1fd7ef8c8a7c56186bc3e76d85bf6faae1611
Muhammad Abdoola <momoawsome55@gmail.com>
2024-06-09 02:33:12 +0200

    build project section and styles and put dummie projects

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Hero from "@/components/Hero";
 import {FloatingNav} from "@/components/ui/FloatingNav";
 import {FaHome} from "react-icons/fa";
 import Grid from "@/components/Grid";
+import RecentProjects from "@/components/RecentProjects";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
             <FloatingNav navItems={[{name: 'Home', link: '/', icon: <FaHome/>}]}/>
             <Hero />
             <Grid/>
+            <RecentProjects/>
         </div>
       </main>
   );
